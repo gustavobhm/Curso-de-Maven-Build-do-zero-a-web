993b43620cb08e34c707c175aee7b3c24ef8ebe6
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-01-15 09:58:18 -0200

    Aula 6 - Projeto web

--- lojaweb/src/main/java/lojaweb/ContatoServlet.java
+++ lojaweb/src/main/java/lojaweb/ContatoServlet.java
@@ -20,7 +20,7 @@ public class ContatoServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter writer = resp.getWriter();
-		writer.println("<html><h2>Entre em Contato</h2></html>");
+		writer.println("<html><h2>Bata um papo conosco</h2></html>");
 		writer.close();
 	}
 }
